d8c79862ca15d4532a8445a6d35bf6c03f05c162
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-15 11:20:28 +0530

    Created a _quantity property in OrderViewModel.kt to hold value of quantity of cupcakes.

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.cupcake.model
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class OrderViewModel : ViewModel() {
+    private val _quantity = MutableLiveData<Int>(0)
 }
