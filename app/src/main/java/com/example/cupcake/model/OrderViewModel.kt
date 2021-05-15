d87f21aa616ab8f4278ab1963783cbd9796e5839
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-15 11:23:35 +0530

    Created a backing property for _quantity property.

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.cupcake.model
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class OrderViewModel : ViewModel() {
     private val _quantity = MutableLiveData<Int>(0)
+    val quantity : LiveData<Int> get() = _quantity
+
     private val _flavor = MutableLiveData<String>("")
     private val _date = MutableLiveData<String>("")
     private val _price = MutableLiveData<Double>(0.0)
