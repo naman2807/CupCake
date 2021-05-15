25c50af5c4e455746efa9aa60c705a60e3142cfe
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-15 11:21:59 +0530

    Created a _date property in OrderViewModel.kt to hold value of date of delivery of cupcakes.

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -6,4 +6,5 @@ import androidx.lifecycle.ViewModel
 class OrderViewModel : ViewModel() {
     private val _quantity = MutableLiveData<Int>(0)
     private val _flavor = MutableLiveData<String>("")
+    private val _date = MutableLiveData<String>("")
 }
