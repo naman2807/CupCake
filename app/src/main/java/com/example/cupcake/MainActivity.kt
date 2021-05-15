51c0be640201247f90e5681c6106f62ebde8fec3
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-15 11:07:59 +0530

    In MainActivity.kt, override the onCreate() method to set up the navigation controller. Get an instance of NavController from the NavHostFragment.

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -15,9 +15,26 @@
  */
 package com.example.cupcake
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController : NavController = navHostFragment.findNavController()
+
+//        This will do the following: Show a title in the app bar based off of the destination's label,
+//        and display the Up button whenever you're not on a top-level destination.
+        setupActionBarWithNavController(navController)
+    }
+
+
+}
