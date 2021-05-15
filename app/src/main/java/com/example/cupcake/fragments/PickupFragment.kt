47c39b792adfe4ad182fdaba6fee6fe91c9cbad9
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-15 11:34:38 +0530

    In PickupFragment.kt class, get a reference to the shared view model as a class variable.

--- app/src/main/java/com/example/cupcake/fragments/PickupFragment.kt
+++ app/src/main/java/com/example/cupcake/fragments/PickupFragment.kt
@@ -21,14 +21,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.R
 import com.example.cupcake.databinding.FragmentPickupBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
  * [PickupFragment] allows the user to choose a pickup date for the cupcake order.
  */
 class PickupFragment : Fragment() {
+    private val sharedViewModel : OrderViewModel by activityViewModels()
 
     // Binding object instance corresponding to the fragment_pickup.xml layout
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
