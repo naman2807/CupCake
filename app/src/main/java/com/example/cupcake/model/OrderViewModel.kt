bbf85251363af2f6aeb658251839021167ea3ebb
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-15 11:22:41 +0530

    Created a _price property in OrderViewModel.kt to hold value of price of cupcakes.

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -7,4 +7,5 @@ class OrderViewModel : ViewModel() {
     private val _quantity = MutableLiveData<Int>(0)
     private val _flavor = MutableLiveData<String>("")
     private val _date = MutableLiveData<String>("")
+    private val _price = MutableLiveData<Double>(0.0)
 }
