73929d85200d15778ee2e726ba689a217c1f6ccb
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-17 14:17:20 +0530

    Within the sendOrder() method, construct the order summary text. Create the formatted order_details string by getting the order quantity, flavor, date, and price from the shared view model.

--- app/src/main/java/com/example/cupcake/fragments/SummaryFragment.kt
+++ app/src/main/java/com/example/cupcake/fragments/SummaryFragment.kt
@@ -59,7 +59,13 @@ class SummaryFragment : Fragment() {
      * Submit the order by sharing out the order details to another app via an implicit intent.
      */
     fun sendOrder() {
-        Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
+        val orderSummary = getString(
+                R.string.order_details,
+                sharedViewModel.quantity.value.toString(),
+                sharedViewModel.flavor.value.toString(),
+                sharedViewModel.date.value.toString(),
+                sharedViewModel.price.value.toString()
+        )
     }
 
     fun cancelOrder(){
